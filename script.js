85023bac5a70c5615807abf6dfb0f219f75a7ec9
vaibha987 <v2goyal95@gmail.com>
2024-08-25 11:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+let totalCost=0
+for(let i=0;i<=arr.length;i++){
+arr.sort((a,b)=>{
+   return a-b
+})
+let first=arr.shift()
+let second=arr.shift()
+let cost=first+second
+arr.push(cost)
+totalCost=totalCost+cost
   
 }
 
